3905c7e5dd6eb1f95cc08be74cdce379f57dd6f2
JhonatanHern <jhonatanhernandez998@gmail.com>
2019-03-30 10:55:55 -0400

    animations & mailchimp integrated

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -247,3 +247,34 @@ jQuery(function($){
 
 });
 
+function v_js_elementInViewport(el) {
+  if ( !el ) {
+    return false
+  }
+  var top = el.offsetTop;
+  var left = el.offsetLeft;
+  var width = el.offsetWidth;
+  var height = el.offsetHeight;
+    while(el.offsetParent) {
+    el = el.offsetParent;
+    top += el.offsetTop;
+    left += el.offsetLeft;
+  }
+  return (
+    top < (window.pageYOffset + window.innerHeight) &&
+    left < (window.pageXOffset + window.innerWidth) &&
+    (top + height) > window.pageYOffset &&
+    (left + width) > window.pageXOffset
+  );
+}
+var v_js_targets = document.getElementsByClassName('j-animate')
+function v_js_check() {
+  for (var i = v_js_targets.length - 1; i >= 0; i--) {
+    if(v_js_elementInViewport(v_js_targets[i]))
+      v_js_targets[i].classList.add('j-animated')
+    else
+      v_js_targets[i].classList.remove('j-animated')
+  }
+}
+document.addEventListener("scroll",v_js_check)
+document.addEventListener("DOMContentLoaded",v_js_check)
